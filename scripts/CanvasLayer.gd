19001849019df27b370493766e510b6478385fb2
Hannelore Lübeck <honeycoffeeandmilk@gmail.com>
2023-04-06 16:03:50 +0200

    modified landing screen

--- scripts/CanvasLayer.gd
+++ scripts/CanvasLayer.gd
@@ -3,8 +3,8 @@ extends "res://scripts/menu.gd"
 signal play_pressed
 signal settings_pressed
 
-#func _ready():
-#	self.slide_in()
+func _ready():
+	$AnimationPlayer.play("StartButtonWiggle")
 
 
 func _on_Button1_pressed():
@@ -13,3 +13,18 @@ func _on_Button1_pressed():
 
 func _on_Button2_pressed():
 	emit_signal("settings_pressed")
+
+
+func _on_StartButton_pressed():
+	emit_signal("play_pressed")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "slide_in":
+		$AnimationPlayer.play("StartButtonWiggle")
+	pass # Replace with function body.
+
+
+func _on_SettingsButton_pressed():
+	emit_signal("settings_pressed")
+	pass # Replace with function body.
--- scripts/GameMenu.gd
+++ scripts/GameMenu.gd
@@ -3,21 +3,21 @@ extends Control
 signal read_sound
 
 func _ready():
-	$MainMenu.slide_in()
 	$Settings.set_offset(Vector2(576, 0))
 	emit_signal("read_sound")
 
 
-func _on_MainMenu_settings_pressed():
-	$MainMenu.slide_out()
-	$Settings.slide_in()
-
-
 func _on_Settings_back_button():
 	$Settings.slide_out()
-	$MainMenu.slide_in()
+	$mainMenu.slide_in()
 
 
-func _on_MainMenu_play_pressed():
+func _on_mainMenu_play_pressed():
 	Global.goto_scene("res://scenes/LevelSelection.tscn")
+	pass # Replace with function body.
 
+
+func _on_mainMenu_settings_pressed():
+	$mainMenu.slide_out()
+	$Settings.slide_in()
+	pass # Replace with function body.
